ab0b432a74150d0fb189aef3bd30fc30b452dd31
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-03-01 15:17:18 -0500

    Create Consumables.h

---
+++ ConsoleRPG/Classes/Items/Consumables/Consumables.h
@@ -0,0 +1,22 @@
+#ifndef	CONSUMABLES_H
+#define CONSUMABLES_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Consumables
+{
+	private:
+		string consumablesName;
+		string consumablesDescription;
+		
+	
+		
+	public:
+		Consumables();
+		~Consumables();
+};
+
+#endif
