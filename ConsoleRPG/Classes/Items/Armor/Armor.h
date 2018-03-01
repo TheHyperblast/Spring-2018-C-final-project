df0bba8c23c0bde1bc13b827e74090a4fa835b12
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-03-01 15:15:48 -0500

    Create Armor.h

---
+++ ConsoleRPG/Classes/Items/Armor/Armor.h
@@ -0,0 +1,24 @@
+#ifndef	ARMOR_H
+#define ARMOR_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Armor
+{
+	private:
+		string armorName;
+		string armorDescription;
+		
+		double defense;
+		double armorValue; 
+	
+	public:
+		Armor();
+		
+		
+};
+
+#endif
