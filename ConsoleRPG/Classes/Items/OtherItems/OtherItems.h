ea073a25c79fb950eacbd28d3326772f93519011
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-03-01 15:18:13 -0500

    Create OtherItems.h

---
+++ ConsoleRPG/Classes/Items/OtherItems/OtherItems.h
@@ -0,0 +1,21 @@
+#ifndef	OTHERITEMS_H
+#define OTHERITEMS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class OtherItems
+{
+	private:
+		string otherItemsName;
+		string otherItemsDescription;
+		
+		double  otherItemsValues;
+		
+	public:
+		OtherItems();
+};
+
+#endif
