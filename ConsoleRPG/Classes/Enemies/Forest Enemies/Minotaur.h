3f7fa03b61630185b047c78deceb8684bfd3dcbe
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:42:25 -0400

    Create Minotaur.h

---
+++ ConsoleRPG/Classes/Enemies/Forest Enemies/Minotaur.h
@@ -0,0 +1,21 @@
+#ifndef MINOTAUR_H 
+#define MINOTAUR_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Minotaur : public Enemy
+{
+	private:
+		
+	public:
+		Minotaur();
+};
+
+#endif
