83868964d06ccc4099c89ff1758809df5144179c
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:40:07 -0400

    Create Naga.h

---
+++ ConsoleRPG/Classes/Enemies/Desert Enemies/Naga.h
@@ -0,0 +1,21 @@
+#ifndef NAGA_H 
+#define NAGA_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Naga : public Enemy
+{
+	private:
+		
+	public:
+		Naga();
+};
+
+#endif
