84d907ab2785e4571fb545c4f38e2d77d6519573
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:22:53 -0400

    Update Armor.cpp

--- ConsoleRPG/CPP files/Armor.cpp
+++ ConsoleRPG/CPP files/Armor.cpp
@@ -4,3 +4,25 @@ Armor::Armor()
 {
 	
 }
+
+Armor::~Armor()
+{
+	
+}
+
+// Functions
+
+// Accessors
+string Armor::getArmorName()
+{ return armorName;}
+
+string Armor::getArmorDescription()
+{ return armorDescription;}
+
+// Mutators
+
+void Armor::setArmorName(string armorName)
+{ this -> armorName = armorName;}
+
+void Armor::setArmorDescription(string armorDescription)
+{ this -> armorDescription = armorDescription;}
