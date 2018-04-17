8ce037ce27893ba6ceca3ab4a5fd684900be13b0
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:37:40 -0400

    Create Drider.h

---
+++ ConsoleRPG/Classes/Enemies/Cave Enemies/Drider.h
@@ -0,0 +1,18 @@
+#ifndef DRIDER_H
+#define DRIDER_H 
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+class Drider : public Enemy
+{
+	private:
+	
+	public:
+		Drider();
+};
+#endif
