7d449083aaa1afdeec02339f8aa5958936fc942d
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:39:46 -0400

    Create Harpy.h

---
+++ ConsoleRPG/Classes/Enemies/Desert Enemies/Harpy.h
@@ -0,0 +1,20 @@
+#ifndef HARPY_H 
+#define HARPY_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Harpy : public Enemy
+{
+	private:
+		
+	public:
+		Harpy();
+};
+#endif
