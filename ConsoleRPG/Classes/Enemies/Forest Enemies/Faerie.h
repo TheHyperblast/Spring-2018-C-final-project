fe17aff41ed23b7c11aac09d629ffb5fae97af5b
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:42:00 -0400

    Create Faerie.h

---
+++ ConsoleRPG/Classes/Enemies/Forest Enemies/Faerie.h
@@ -0,0 +1,21 @@
+#ifndef FAERIE_H
+#define FAERIE_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Faerie : public Enemy
+{
+	private:
+		
+	public:
+		Faerie();
+};
+
+#endif
