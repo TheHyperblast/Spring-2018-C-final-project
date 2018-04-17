26ea4276920c9de7a0327b0f0828a5ef01a67eea
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:41:15 -0400

    Create Imp.h

---
+++ ConsoleRPG/Classes/Enemies/Explore Enemies/Imp.h
@@ -0,0 +1,20 @@
+#ifndef	IMP_H
+#define IMP_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Imp : public Enemy
+{
+	private:
+	
+	public:
+		Imp();	
+};
+#endif
