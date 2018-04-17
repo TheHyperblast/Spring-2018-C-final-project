74659b3e434609e975221a4ef22b16bc8ae0ecf4
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:40:53 -0400

    Create Goblin.h

---
+++ ConsoleRPG/Classes/Enemies/Explore Enemies/Goblin.h
@@ -0,0 +1,21 @@
+#ifndef	GOBLIN_H
+#define GOBLIN_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Goblin : public Enemy
+{
+	private:
+	
+	public:
+		Goblin();
+};
+
+#endif
