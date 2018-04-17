a05b0ec3f1412080070b4365648e698efcf22534
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:37:16 -0400

    Create Dragon.h

---
+++ ConsoleRPG/Classes/Enemies/Cave Enemies/Dragon.h
@@ -0,0 +1,21 @@
+#ifndef DRAGON_H
+#define DRAGON_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Dragon : public Enemy
+{
+	private:
+		
+	public:
+		Dragon();
+};
+
+#endif
