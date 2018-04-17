a32414acd26621d10aa896b327ef99ac32c1449c
TheHyperblast <ejohnmichaelkuczynski@gmail.com>
2018-04-17 14:43:06 -0400

    Create Shark.h

---
+++ ConsoleRPG/Classes/Enemies/Lake Enemies/Shark.h
@@ -0,0 +1,21 @@
+#ifndef SHARK_H 
+#define SHARK_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+#include "Enemy.h"
+
+using namespace std;
+
+class Shark : public Enemy
+{
+	private:
+		
+	public:
+		Shark();
+};
+
+#endif
